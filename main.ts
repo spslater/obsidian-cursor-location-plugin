03a83838a1217ab88f9185d87c4773227174a08e
Sean Slater <seanslater@whatno.io>
2021-06-30 22:58:56 -0400

    Add Settings Tab
    Add Settings to tab the lets user pick number of cursors they want
    to display info about.

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
-import { App, Editor, EditorPosition, EditorSelection, MarkdownView, Plugin } from 'obsidian';
+import { 
+	App,
+	Editor,
+	EditorPosition,
+	EditorSelection,
+	MarkdownView,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	TextComponent,
+} from 'obsidian';
 import * as CodeMirror from "codemirror";
+
+interface CursorLocationSettings {
+	numberCursors: number;
+}
+
+const DEFAULT_SETTINGS: CursorLocationSettings = {
+	numberCursors: 1
+}
 
 export default class CursorLocation extends Plugin {
 	private cursorStatusBar: HTMLElement;
+	settings: CursorLocationSettings;
 
 	async onload() {
 		console.log('loading Cursor Location plugin');
@@ -11,7 +30,22 @@ export default class CursorLocation extends Plugin {
 			cm.on("cursorActivity", this.updateCursor);
 		});
 
-		this.updateCursor(this.getEditor())
+		await this.loadSettings();
+		this.addSettingTab(new CursorLocationSettingTab(this.app, this));
+
+		this.updateCursor(this.getEditor());
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
 	}
 
 	private getEditor(): Editor {
@@ -28,7 +62,8 @@ export default class CursorLocation extends Plugin {
 			if (!this.cursorStatusBar) {
 				this.cursorStatusBar = this.addStatusBarItem();
 			}
-			let selections = ""
+			let selections = "";
+			// let selections: EditorSelection[] = this.getEditor()?.listSelections()
 			// this.getEditor()?.listSelections()?.forEach(selection => {
 			// 	selections += "(" + this.cursorString(selection.anchor) + "//" + this.cursorString(selection.head) + ")"
 			// })
@@ -36,5 +71,75 @@ export default class CursorLocation extends Plugin {
 				this.cursorString(cursor) + "/" + editor.lineCount() + " " + selections
 			);
 		}
+	}
+}
+
+
+class CursorLocationSettingTab extends PluginSettingTab {
+	plugin: CursorLocation;
+
+	constructor(app: App, plugin: CursorLocation) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		let { containerEl } = this;
+
+		containerEl.empty();
+
+		let cursorNumber = new Setting(containerEl)
+			.setName('# of Cursor')
+			.setDesc('Number of cursors to display in the status bar.')
+			.addText(text => text
+				.setValue(this.plugin.settings?.numberCursors?.toString())
+				.onChange(async (value) => {
+					let parsedValue = parseInt(value);
+					if (!isNaN(parsedValue)) {
+						console.log(
+							"Updating number of cursors to display to " +
+							parsedValue
+						)
+						warningSection.setText('')
+						this.plugin.settings.numberCursors = parsedValue;
+						await this.plugin.saveSettings();
+					} else {
+						console.log(
+							"Number of cursors to display not updated."+
+							"Unable to parse new value into integer: " +
+							value
+						)
+						warningSection.setText(
+							value +
+							" is not a number, unable to save."
+						)
+					}
+				}));
+
+		let warningSection = containerEl.createEl('p', {
+			text: '',
+			attr: { "style": "color:red" },
+		});
+
+		new Setting(containerEl)
+			.setName('Reset')
+			.setDesc(
+				'Reset number of cursors to ' +
+				DEFAULT_SETTINGS.numberCursors
+			)
+			.addButton(cb => cb
+				.setButtonText("Reset")
+				.onClick(async (evt) => {
+					let textComponent: TextComponent = 
+						cursorNumber.components[0] as TextComponent;
+					textComponent.setValue(
+						DEFAULT_SETTINGS.numberCursors.toString()
+					)
+					warningSection.setText('')
+					this.plugin.settings.numberCursors =
+						DEFAULT_SETTINGS.numberCursors;
+					await this.plugin.saveSettings()
+				})
+			);
 	}
 }
